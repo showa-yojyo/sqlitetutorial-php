bb11d9ebc4adc5bab83e005940e953afbde6afc1
showa_yojyo <yojyo@hotmail.com>
2022-11-05 23:38:02 +0900

    document.php: use stream_get_contents

--- document.php
+++ document.php
@@ -14,8 +14,8 @@ $documentId = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 // read documet from the database
 $doc = $sqlite->readDoc($documentId);
 if ($doc != null) {
-    header("Content-Type:" . $doc['mime_type']);
-    echo $doc['doc'];
+    header("Content-Type: " . $doc['mime_type']);
+    echo stream_get_contents($doc['doc']);
 } else {
     echo 'Error loading document ' . $documentId;
 }
